323e01f2bc3a47b9196fb8832599d626e0003399
sunwaylive <sunwayliving@gmail.com>
2016-05-05 15:14:13 +0800

    use blocking way to implement communication, and can createroom successfully

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-BOOST_INCLUDE_DIR = "/home/sunwayliving/data/boost_1_60_0"
-BOOST_LIB_DIR = /home/sunwayliving/data/lib
+BOOST_INCLUDE_DIR = "/home/sunwayliving/boost_1_60_0"
+BOOST_LIB_DIR = /home/sunwayliving/lib
 
 ALL_INCLUDE = -I $(BOOST_INCLUDE_DIR) -I ./ -I /usr/include/ -I /usr/include/c++/4.9.3/
 
 CC = g++
 DEBUG = -g
 CFLAGS = -Wall -c $(DEBUG) -std=c++11
-LFLAGS = -pthread -L$(BOOST_LIB_DIR) -lboost_thread -lboost_system
+LFLAGS = -pthread -L$(BOOST_LIB_DIR) -lboost_thread -lboost_system -lrt
 
 all: server
 
--- server.cpp
+++ server.cpp
@@ -4,14 +4,66 @@
 #include <utility>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind.hpp>
+#include <string.h>
+#include <map>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
 
 using boost::asio::ip::tcp;
+using namespace std;
+
+typedef boost::shared_ptr<tcp::socket> socket_ptr;
+
+struct Room
+{
+    int room_id;
+    int max_player_num;
+    std::vector<socket_ptr> player_list;
+
+    Room(int rid, int maxPlayerNum)
+    : room_id(rid), max_player_num(maxPlayerNum)
+    {
+    }
+};
+
 const std::string SERVER_IP = "119.29.25.185";
 const std::string PORT = "3008";
 const int max_length = 1024;
+int gRoomId = 0;
+vector<Room*> gRoomList;
 
-void session(tcp::socket sock)
+void create_room(socket_ptr sock, int playerNum)
 {
+    std::cout<<"create room for " << playerNum <<" players!" <<std::endl;
+    Room *new_room = new Room(gRoomId, playerNum);
+    new_room->player_list.push_back(sock);
+    //std::cout<<"In sub thread" <<sock-> <<endl;
+    //printf("%x", sock);
+    gRoomList.push_back(new_room);
+    gRoomId++; //increase the global room id
+
+    //tell the client that room is created successfully
+    string toSend = "CREATEROOM@" + std::to_string(gRoomId) + "\n"; 
+    boost::asio::write(*sock, boost::asio::buffer(toSend, toSend.length()));
+}
+
+void handle_message(socket_ptr sock, std::string msg)
+{
+    std::cout<<"I have received msg: " <<msg <<std::endl;
+    std::size_t found = msg.find("CREATEROOM");
+    if(found != std::string::npos)
+    {
+       int playerNum = std::stoi(msg.substr(strlen("CREATEROOM") + 1));
+       create_room(sock, playerNum);
+    }
+}
+
+void client_session(socket_ptr sock)
+{
+    std::cout<<"A client has connected to me!" <<std::endl;
     try
     {
         for(;;)
@@ -19,14 +71,15 @@ void session(tcp::socket sock)
             char data[max_length] = "";
 
             boost::system::error_code error;
-            size_t length = sock.read_some(boost::asio::buffer(data), error);
+            size_t length = sock->read_some(boost::asio::buffer(data), error);
             if(error == boost::asio::error::eof)
                 break; //connection closed cleanly by peer
             else if(error)
                 throw boost::system::system_error(error); //some other error
 
-            std::cout<<"I have received msg: " <<std::string(data) <<std::endl;
-            boost::asio::write(sock, boost::asio::buffer(data, length));
+            handle_message(sock, std::string(data));
+            std::cout<<"recv from client length: " << length <<std::endl;
+            //boost::asio::write(sock, boost::asio::buffer(data, length));
         }
     }
     catch(std::exception &e)
@@ -40,10 +93,18 @@ void server(boost::asio::io_service &io_service, unsigned short port)
     tcp::acceptor acp(io_service, tcp::endpoint(tcp::v4(), port));
     for(;;)
     {
-        tcp::socket sock(io_service);
-        acp.accept(sock);
+        //tcp::socket sock(io_service);
+        socket_ptr sock(new tcp::socket(io_service));
+        acp.accept(*sock);
+        //printf("%x", sock);
+
+        boost::thread(boost::bind(client_session, sock));
+
         //after std::move the sock will be empty
-        std::thread(session, std::move(sock)).detach();
+        //std::thread(functionPtr, args)
+        
+        //std::thread(session, std::move(sock)).detach();
+        //after seperate the socket into a new thread, the main thread will go on listening to clients
     }
 }
 
